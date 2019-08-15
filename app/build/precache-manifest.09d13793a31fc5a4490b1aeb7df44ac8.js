dc67e1ba0a7903662e84e1f4e0857762035e12f6
root <root@vps719170.ovh.net>
2019-08-16 01:51:13 +0200

    added build to deploy it

---
+++ app/build/precache-manifest.09d13793a31fc5a4490b1aeb7df44ac8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a841d5f68fb8edf73f050cfa18214d1c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f96cf6e87ebc611fc3b0",
+    "url": "/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "38edd1b22b97ddb4ca26",
+    "url": "/static/js/2.280580c9.chunk.js"
+  },
+  {
+    "revision": "f96cf6e87ebc611fc3b0",
+    "url": "/static/js/main.49a0c479.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
