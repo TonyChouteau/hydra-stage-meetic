1a38f2c60ba23ca4e77309e67496ec6f3ba979cf
root <root@vps719170.ovh.net>
2019-08-17 12:16:20 +0200

    git add build/* -f

---
+++ app/build/precache-manifest.0a612774e3a0c106de96e07caedcf205.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "722f21fc1be4867e6ceefd018d820cd6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "96e44beeacd06345589e",
+    "url": "/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "e246abd2e24b86012604",
+    "url": "/static/js/2.f538daa4.chunk.js"
+  },
+  {
+    "revision": "96e44beeacd06345589e",
+    "url": "/static/js/main.d63ccf70.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
