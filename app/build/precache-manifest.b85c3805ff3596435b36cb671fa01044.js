ab2b519d9d22bb169b854bece7c60a936cfed653
root <root@vps719170.ovh.net>
2019-08-19 02:13:15 +0200

    again build js missing added

---
+++ app/build/precache-manifest.b85c3805ff3596435b36cb671fa01044.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e01af83d957ab531327f57d4ba587eda",
+    "url": "/index.html"
+  },
+  {
+    "revision": "adf4deb2a812cd933d3a",
+    "url": "/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "bb7bdcf7bbcbd6f720cd",
+    "url": "/static/js/2.5d001b11.chunk.js"
+  },
+  {
+    "revision": "adf4deb2a812cd933d3a",
+    "url": "/static/js/main.a438419d.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
